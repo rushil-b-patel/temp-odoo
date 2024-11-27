98497e0cba342d9e409b2127b978cabfcfb55fb3
Rushil <rushil13579@gmail.com>
2024-11-27 19:34:17 +0530

    done

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ dotenv.config();
 const FROMEMAIL = process.env.FROMEMAIL;
 const TOEMAIL = process.env.TOEMAIL;
 const PASSWORD = process.env.PASSWORD;
+const NAME = process.env.NAME;
+
+const emailText = `
+Respected Ma'am,
+
+I am writing to follow up on my Coderbyte test for the Software Developer Intern position(applied on 22nd Oct). I successfully completed the test on Oct 24, 2024.
+
+I would appreciate it if you could provide an update on the status of my application. Any feedback or information regarding the next steps in the hiring process would be greatly helpful.
+
+Thank you for your time and consideration.
+
+Sincerely,  
+${NAME}
+`;
 
 async function sendEmail() {
   try {
@@ -20,8 +34,8 @@ async function sendEmail() {
     const mailOptions = {
       from: FROMEMAIL,
       to: TOEMAIL,
-      subject: "Scheduled Email",
-      text: "This is an automated email sent using GitHub Actions.",
+      subject: "Follow-Up on Coderbyte Test for Software Developer Intern Position",
+      text: emailText,
     };
 
     await transporter.sendMail(mailOptions);
